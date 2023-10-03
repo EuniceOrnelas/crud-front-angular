30b8da3b81d2edc4b2f403218c66fc2de8e43112
Eunice <eunice.ornelas@2bcore.com>
2023-10-03 12:54:21 -0600

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,51 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from './employee.service';
 
+// Define una interfaz para representar la estructura de un objeto Employee.
 interface Employee {
   id: number;
   name: string;
-  complated: boolean;
+  completed: boolean;
 }
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  employees: Employee[];
-  employee: string;
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor(
-    private employeeService: EmployeeService,
-  ) {
+  // Propiedades del componente
+  employees: Employee[]; // Almacena la lista de empleados recuperada del servicio.
+  employee: string; // Almacena el nombre del nuevo empleado a agregar.
+
+  constructor(private employeeService: EmployeeService) {
+    // Inicialización de propiedades
     this.employees = [];
     this.employee = '';
   }
 
+  // Título de la aplicación
   title = 'employee-ui';
 
+  // Método que se ejecuta cuando el componente se inicializa
   ngOnInit() {
+    // Suscribe al servicio para obtener la lista de empleados y la asigna a la propiedad employees.
     this.employeeService.getEmployees().subscribe((data) => {
       console.log(data);
       this.employees = data as Employee[];
     });
   }
   
+  // Método para agregar un empleado
   addEmployee(employee: string) {
+    // Llama al servicio para agregar un empleado y se suscribe a la respuesta.
     this.employeeService.addEmployee(employee).subscribe();
+    // Limpia el campo de entrada del nombre del empleado después de agregarlo.
     this.employee ='';
   }
 
-
+  // Método para eliminar un empleado
   deleteEmployee(id: number) {
+    // Llama al servicio para eliminar un empleado por su ID y se suscribe a la respuesta.
     this.employeeService.deleteEmployee(id).subscribe((data) => {
       console.log(data);
     });
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -6,22 +6,40 @@ import { map } from 'rxjs';
   providedIn: 'root',
 })
 export class EmployeeService {
-  host = 'http://localhost:3000/api';
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // URL base del servidor de la API
+  private host = 'http://localhost:3000';
+
   constructor(private http: HttpClient) {}
 
+  /**
+   * Obtiene la lista de empleados desde el servidor.
+   * @returns Un observable que emite la lista de empleados.
+   */
   getEmployees() {
+    // Realiza una solicitud GET al servidor y mapea la respuesta a formato JSON.
     return this.http.get(`${this.host}/employee`).pipe(map((res) => res));
   }
 
+  /**
+   * Agrega un nuevo empleado al servidor.
+   * @param employee El nombre del empleado a agregar.
+   * @returns Un observable que emite la respuesta del servidor.
+   */
   addEmployee(employee: string) {
+    // Realiza una solicitud POST al servidor con los datos del nuevo empleado.
     return this.http.post(`${this.host}/employee`, {
       name: employee,
-      complated: false,
+      completed: false,
     });
   }
 
+  /**
+   * Elimina un empleado del servidor por su ID.
+   * @param id El ID del empleado a eliminar.
+   * @returns Un observable que emite la respuesta del servidor.
+   */
   deleteEmployee(id: number) {
+    // Realiza una solicitud DELETE al servidor para eliminar al empleado por su ID.
     return this.http.delete(`${this.host}/employee/${id}`);
   }
 }
